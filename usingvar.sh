ea93a89d7323488aa32f13db727ebffd5d884a7f
Divakar <dvkr22@gmail.com>
2022-10-01 06:06:13 -0400

    new

--- usingvar.sh
+++ usingvar.sh
@@ -8,6 +8,7 @@ remove=`rm -rf top.txt`
 t=`top | head -10 > top.txt`
 file=`chown devops:devops top.txt`
 pwd=`pwd`
+echo
 echo Removing Old file
 echo
 $remove
@@ -15,7 +16,9 @@ echo Working on $pwd
 echo
 echo Creating file named top.txt and adding values
 $t
+echo
 echo Changing Permissions
 $file
+echo
 echo End of script
 
