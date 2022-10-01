4afd6258cdcc22f51bdeb8a54b003d25ed5cd296
Divakar k <89780198+DivakarK22@users.noreply.github.com>
2022-10-01 16:30:48 +0530

    add

--- ifelsegit.sh
+++ ifelsegit.sh
@@ -8,14 +8,14 @@ gitpull=`git pull | awk '{print $1}'`
 cd $gitdir
 
 echo select the repo
+
 read $gitfolder
+
 echo Checking $gitfolder
 
 cd $gitfolder
 
 echo running git pull
-
- 
 
 if [ "$gitpull" == 'Already' ]
 then
