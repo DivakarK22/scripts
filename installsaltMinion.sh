01e1dfe7f032c52d219912e46e579dde83e479e5
Divakar k <89780198+DivakarK22@users.noreply.github.com>
2022-10-30 15:42:15 +0530

    added node_exp

--- installsaltMinion.sh
+++ installsaltMinion.sh
@@ -3,7 +3,7 @@ curl -L https://bootstrap.saltstack.com -o install_salt.sh
 sh install_salt.sh -P
 
 cat <<EOF > /etc/salt/minion_id
-master: 192.168.1.61
+$HOSTNAME
 EOF
 
 systemctl start salt-minion.service
