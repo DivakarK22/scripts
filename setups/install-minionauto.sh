35c5eb9bba4e54b992306f7d16446782463b60f4
Divakar k <89780198+DivakarK22@users.noreply.github.com>
2022-11-01 19:31:45 +0530

    changes

--- setups/install-minionauto.sh
+++ setups/install-minionauto.sh
@@ -4,12 +4,14 @@ yum update -y
 
 yum install git -y
 
+cd $HOME
+
 mkdir git
 
 cd git
 
 git clone https://github.com/DivakarK22/scripts.git
 
-cd setups
+cd $HOME/git/scripts/setups
 
 bash installsaltMinion.sh
