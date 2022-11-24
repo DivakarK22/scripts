ed2210bc01e966c94e8e299fd7e6097184fae4f1
divakar <dvkr22@gmail.com>
2022-11-24 22:15:27 +0530

    added

--- local/neworkfile.sh
+++ local/neworkfile.sh
@@ -8,31 +8,30 @@ cd $HOME
 
 git clone https://github.com/DivakarK22/scripts.git
 
-cat <<EOF >> /etc/sysconfig/network-scripts/ifcfg-eth0
-
-TYPE=Ethernet
-BOOTPROTO=none
-# Server IP #
-IPADDR=$1
-# Subnet #
-PREFIX=24
-# Set default gateway IP #
-GATEWAY=192.168.1.1
-# Set dns servers #
-DNS1=192.168.1.1
-DNS2=8.8.8.8
-DNS3=8.8.4.4
-DEFROUTE=yes
-IPV4_FAILURE_FATAL=no
-# Disable ipv6 #
-IPV6INIT=no
-NAME=eth0
-# This is system specific and can be created using 'uuidgen eth0' command #
-DEVICE=eth0
-ONBOOT=yes
-UUID="436bd03f-284e-484e-aadc-3ebe9d60ac10"
-
+cat <<EOF > /etc/sysconfig/network-scripts/ifcfg-eth0
+        TYPE=Ethernet
+        BOOTPROTO=none
+        # Server IP #
+        IPADDR=$1
+        # Subnet #
+        PREFIX=24
+        # Set default gateway IP #
+        GATEWAY=192.168.1.1
+        # Set dns servers #
+        DNS1=192.168.1.1
+        DNS2=8.8.8.8
+        DNS3=8.8.4.4
+        DEFROUTE=yes
+        IPV4_FAILURE_FATAL=no
+        # Disable ipv6 #
+        IPV6INIT=no
+        NAME=eth0
+        # This is system specific and can be created using 'uuidgen eth0' command #
+        DEVICE=eth0
+        ONBOOT=yes
+        UUID="436bd03f-284e-484e-aadc-3ebe9d60ac10"
 EOF
+
 
 cat <<EOF >>/etc/hostname
 $2
