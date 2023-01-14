f7464ae72f5b5ce354c6ed90db5efb7feab2478c
divakar <dvkr22@gmail.com>
2023-01-14 10:53:52 +0530

    added

--- setups/ruby.sh
+++ setups/ruby.sh
@@ -16,12 +16,9 @@ yum install -y yarn
 cd
 rm -rf .rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL
+
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-exec $SHELL
+
 #Install the latest version of Ruby (v2.7.0)
 rbenv install 2.7.0
 #Set Ruby v2.7.0 as the default version for all shells.
