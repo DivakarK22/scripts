7284d20681b38c4b7097864e88637716be598de7
Divakar k <89780198+DivakarK22@users.noreply.github.com>
2023-02-25 14:35:55 +0530

    Update ruby.sh

--- setups/ruby.sh
+++ setups/ruby.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-yum update -y
+sudo yum update -y
 #Install req pkgs
-yum install -y git-core zlib zlib-devel gcc-c++ patch readline readline-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison curl sqlite-devel
+sudo yum install -y git-core zlib zlib-devel gcc-c++ patch readline readline-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison curl sqlite-devel
 
 #Install Nodejs
-curl -sL https://rpm.nodesource.com/setup_12.x | bash
-yum install -y nodejs ruby
+sudo curl -sL https://rpm.nodesource.com/setup_12.x | bash
+sudo yum install -y nodejs ruby
 
 #Install Yarn
-curl -sL https://dl.yarnpkg.com/rpm/yarn.repo -o /etc/yum.repos.d/yarn.repo
-yum install -y yarn
+sudo curl -sL https://dl.yarnpkg.com/rpm/yarn.repo -o /etc/yum.repos.d/yarn.repo
+sudo yum install -y yarn
 
 #Install Ruby 2.7.0
 cd
@@ -18,7 +18,7 @@ rm -rf .rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-
+cd
 #Install the latest version of Ruby (v2.7.0)
 rbenv install 2.7.0
 #Set Ruby v2.7.0 as the default version for all shells.
