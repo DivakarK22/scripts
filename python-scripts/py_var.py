14cc6024514c929cbf0ee3571bd39f47c3a03ddd
Divakar <dvkr22@gmail.com>
2023-03-23 00:22:15 +0530

    Updated py

---
+++ python-scripts/py_var.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python3
+
+first_name = "Divakar"
+last_name = "Kumar"
+
+print("Hello " +first_name+last_name)
+print(last_name.isdigit())
+print(last_name.find("K"))
+print(last_name.endswith("r"))
+print(type(first_name + last_name))
+print(first_name.upper())
+
+age = 23
+dob = 22011999
+
+print("Your age"+" "+str(age)+" "+"Your DOB"+" "+str(dob))
+print(type(dob))
+height = 180.8
+#dob = 22/01/1999
+
+print("Your Height:"+str(height))
+print(type(height))
+
+status = True
+
+print("Are you working currently"+str(status))
+print(type(status))
