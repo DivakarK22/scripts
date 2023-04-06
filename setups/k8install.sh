2bce41b42bc9c9af1cf2841676aa29607945f91a
Divakar <dvkr22@gmail.com>
2023-04-06 19:23:48 +0530

    mod

--- setups/k8install.sh
+++ setups/k8install.sh
@@ -14,11 +14,38 @@ docker rm `docker ps -a -q`
 
 docker rmi `docker images -q`
 
-kubeadm reset
+systemctl disable firewalld; systemctl stop firewalld
+
+swapoff -a; sed -i '/swap/d' /etc/fstab
+
+setenforce 0
+sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
+
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce-19.03.12 
+systemctl enable --now docker
 
 yum remove -y  kubeadm kubectl kubelet kubernetes-cni kube*
 
 yum autoremove
+
+cat >>/etc/yum.repos.d/kubernetes.repo<<EOF
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
 
 rm -rf ~/.kube
 
@@ -49,7 +76,8 @@ sudo firewall-cmd --permanent --add-port=10252/tcp
 sudo firewall-cmd --permanent --add-port=10255/tcp
 sudo firewall-cmd --reload
 
-yum install -y kubeadm* kubelet* kubectl*
+yum install -y kubeadm-1.18.5-0 kubelet-1.18.5-0 kubectl-1.18.5-0
 
+systemctl enable --now kubelet
 
 systemctl enable --now kubelet
