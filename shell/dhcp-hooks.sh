d72a9d78ba83db225865c791c571213d7c7b1826
Divakar <dvkr22@gmail.com>
2023-06-17 14:51:18 +0530

    updating hooks to update named aswell

--- shell/dhcp-hooks.sh
+++ shell/dhcp-hooks.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 
-cd /etc/dhcp
+cd /etc/dhcp/
 
 $PWD
 
 sudo git pull
 
 sudo systemctl restart dhcpd
+
+rm -rf /etc/named.conf
+
+rm -rf /etc/named.rfc1912.zones
+
+rm -rf /var/named/example.fwd
+
+rm -rf /var/named/example.rev
+
+cd /etc/dhcp/named
+
+cp example.fwd /var/named/
+
+cp example.rev /var/named/
+
+chown named.named /var/named/example.fwd
+
+chown named.named /var/named/example.rev
+
+cp named.conf /etc/
+
+cp named.rfc1912.zones /etc/
+
+chown named.named /etc/named.rfc1912.zones
+
+chown named.named /etc/named.conf
+
+systemctl restart named
 
 echo "DONE"
