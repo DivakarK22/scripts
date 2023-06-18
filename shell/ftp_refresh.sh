984e6b379e9ffa0f77b1c0685f3317b8697b4424
Divakar <dvkr22@gmail.com>
2023-06-18 16:51:39 +0530

    added

--- shell/ftp_refresh.sh
+++ shell/ftp_refresh.sh
@@ -5,6 +5,8 @@ sudo yum remove nfs-utils -y
 
 sudo yum install nfs-utils -y
 
+mv /etc/exports /etc/exports.bk
+
 systemctl restart rpcbind
 systemctl restart nfs-server
 systemctl restart nfs-lock
