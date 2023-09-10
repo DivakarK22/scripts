9f1049ad5dcc7623f715bdb121d8d069713720e7
Your Name <you@example.com>
2023-09-10 23:18:40 +0530

    added sh script to clone all repo from one user id

--- shell/gitrepopullall.sh
+++ shell/gitrepopullall.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-read -p "Enter the repo username to clone" repo_name
+
+read -p "Enter the repo username to clone:" repo_name
 mkdir /$HOME/git
-echo cd /$HOME/git
-curl https://github.com/$repo_name\?tab\=repositories | grep "<a href=" | grep itemprop | awk '{print $2}' > /$HOME/git/garbage.txt
-pwd
+
+curl https://github.com/$repo_name?tab=repositories | grep "<a href=" | grep itemprop | awk '{print $2}' > /$HOME/git/garbage.txt
 cut -c6- /$HOME/git/garbage.txt > /$HOME/git/repo_tmp.txt
 cat /$HOME/git/repo_tmp.txt | sed 's/"//g' > /$HOME/git/repo.txt
 rm -rf /$HOME/git/garbage.txt /$HOME/git/repo_tmp.txt
+
 file="/$HOME/git/repo.txt"
+
 while read -r line; do
-cd /$HOME/temp/
+cd /$HOME/git
 git clone https://github.com/$line.git
 done <$file
+
 echo exit
 
 
