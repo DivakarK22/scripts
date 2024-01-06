d2361e3ce691d32edd2440359379ef5c9ad0a8d2
Your Name <you@example.com>
2024-01-06 15:46:10 +0530

    updated

--- shell/gitrepopullall.sh
+++ shell/gitrepopullall.sh
@@ -6,8 +6,14 @@ read -p "Enter the repo username to clone:" repo_name
 #Create git under user home dir
 mkdir /$HOME/git
 
+<<<<<<< HEAD
 #INSTALL GIT
 yum install git -y
+=======
+#Create a folder
+mkdir /$HOME/git/$repo_name
+
+>>>>>>> c1e5666 (updated)
 #Collecting repo details
 curl https://github.com/$repo_name?tab=repositories | grep "<a href=" | grep itemprop | awk '{print $2}' > /$HOME/git/garbage.txt
 cut -c6- /$HOME/git/garbage.txt > /$HOME/git/repo_tmp.txt
@@ -18,7 +24,7 @@ file="/$HOME/git/repo.txt"
 
 #Cloning repo's
 while read -r line; do
-cd /$HOME/git
+cd /$HOME/git/$repo_name
 git clone https://github.com/$line.git
 done <$file
 
