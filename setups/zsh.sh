0b0808bf6cf5393c00a1629a3df6608e65c83dbb
Divakar K <dvkr22@gmail.com>
2024-06-04 00:07:11 +0530

    added

--- setups/zsh.sh
+++ setups/zsh.sh
@@ -8,8 +8,8 @@ yum install wget curl git -y | true
 dnf install wget curl git -y | true
 
 echo "Installng zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+bash install.sh
 echo "Cloning ZSH Theme file"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
